1bbf651aeed88755e99cd73e8a9b4b279b2f34b5
Xavier Labeur <xavier.labeur@gmail.com>
2015-01-15 17:04:31 +0100

    Add seeder for default user

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* User table seeder
+*/
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array('email' => 'admin@mydomain.fr', 'password' => Hash::make('123456'), 'firstname' => 'Admin'));
+    }
+}
