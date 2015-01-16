8438c661e95b54587b926c715a9a2279faa611af
Xavier Labeur <xavier.labeur@gmail.com>
2015-01-16 09:58:00 +0100

    Add default.env and edit the readme

---
+++ default.env.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+    'debug'         =>  true,
+    'key_secure'    =>  'changeItForYourSecurity',
+    'site_url'      =>  'http://mydomain.fr',
+
+    'db_config'     =>  'mysql',
+    'db_host'       =>  'localhost',
+    'db_name'       =>  '',
+    'db_user'       =>  '',
+    'db_password'   =>  '',
+
+    'organisation_name'     =>  'My Organisation',
+    'organisation_address'  =>  'My address',
+    'organisation_zipcode'  =>  'XXXXX',
+    'organisation_city'     =>  'My City',
+    'organisation_country'  =>  'My Country',
+    'organisation_siret'    =>  'xxxxxx',
+    'organisation_tva'      =>  'xxxxxx',
+    'organisation_status'   =>  'Status',
+    'organisation_capital'  =>  'xxxx€'
+);
