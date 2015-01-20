8312054f8e97c729d97ebae54121fe66ed341ed5
Xavier Labeur <xavier.labeur@gmail.com>
2015-01-20 12:31:40 +0100

    Fix error path for wkhtmltopdf

--- app/config/packages/barryvdh/laravel-snappy/config.php
+++ app/config/packages/barryvdh/laravel-snappy/config.php
@@ -5,12 +5,12 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltopdf',
+        'binary' => '/usr/bin/wkhtmltopdf',
         'options' => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltoimage',
+        'binary' => '/usr/bin/wkhtmltoimage',
         'options' => array(),
     ),
 
