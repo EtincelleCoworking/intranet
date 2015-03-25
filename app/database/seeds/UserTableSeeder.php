766bcdc7cefe535087d2c9bfb90af068bd5852b9
vagrant <gallet.jason@gmail.com>
2015-03-25 09:25:19 +0000

    changed dbseed admin

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -8,6 +8,6 @@ class UserTableSeeder extends Seeder
     {
         DB::table('users')->delete();
 
-        User::create(array('email' => 'admin@mydomain.fr', 'password' => Hash::make('123456'), 'firstname' => 'Admin', 'role' => 'superadmin'));
+        User::create(array('email' => 'admin@mydomain.fr', 'password' => Hash::make('123456'), 'firstname' => 'Admin', 'lastname' => 'admin', 'bio_short' => '', 'bio_long' => '', 'avatar' => '', 'is_member' => 1, 'twitter' => '', 'website' => '', 'role' => 'superadmin'));
     }
 }
