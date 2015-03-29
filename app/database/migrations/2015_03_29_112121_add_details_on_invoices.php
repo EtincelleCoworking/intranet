eabea269100e4456ef1663a6d2ad26e8d8641e0c
Xavier Labeur <xavier.labeur@gmail.com>
2015-03-29 13:34:50 +0200

    commentaires sur les devis/factures #80 Ajouter la possibilité de mettre un comment sur une facture qui se place au dessus du tableau de détail #69

---
+++ app/database/migrations/2015_03_29_112121_add_details_on_invoices.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDetailsOnInvoices extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('invoices', function(Blueprint $table)
+		{
+			$table->string('details')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('invoices', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
