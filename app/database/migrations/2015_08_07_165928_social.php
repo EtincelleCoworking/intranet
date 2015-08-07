bf2cbc40fcdfd9f7b7ae055a38987ab0eef2721b
Sebastien Hordeaux <shordeaux@waterproof.fr>
2015-08-07 20:01:44 +0200

    member listing layout tweaks

---
+++ app/database/migrations/2015_08_07_165928_social.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Social extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('social_github');
+			$table->string('social_instagram');
+			$table->string('social_linkedin');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
