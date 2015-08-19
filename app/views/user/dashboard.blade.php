5a8b492c1b5f79f2f02d7489aab1cadb387f4f53
Sebastien Hordeaux <shordeaux@waterproof.fr>
2015-08-19 19:00:19 +0200

    ui tweaks

--- app/views/user/dashboard.blade.php
+++ app/views/user/dashboard.blade.php
@@ -9,14 +9,16 @@
                 @include('partials.wall.component')
             </div>
             <div class="col-lg-3">
-                <div class="ibox">
-                    <div class="ibox-title">
-                        <h5>CA du mois</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h1 class="no-margins">{{ number_format($totalMonth ? $totalMonth->total : 0, 0, ',', '.') }}
-                            €</h1>
-                        <small>&nbsp;</small>
+                <div class="widget style1 navy-bg">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <i class="fa fa-money fa-5x"></i>
+                        </div>
+                        <div class="col-xs-8 text-right">
+                            <span> CA du mois </span>
+                            <h2 class="font-bold">
+                                {{ number_format($totalMonth ? $totalMonth->total : 0, 0, ',', '.') }} €</h2>
+                        </div>
                     </div>
                 </div>
 
@@ -38,14 +40,18 @@
                     </div>
                 @endif
 
-                <div class="ibox">
-                    <div class="ibox-title">
-                        <h5>Encours Clients</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h1 class="no-margins">{{ number_format($pending['total'], 0, ',', '.') }}€</h1>
-                        <small>En compte: {{ number_format($on_hold['total'], 0, ',', '.') }}€</small>
-
+                <div class="widget style1 yellow-bg">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <i class="fa fa-pause fa-5x"></i>
+                        </div>
+                        <div class="col-xs-8 text-right">
+                            <span> Encours Clients</span>
+                            <h2 class="font-bold">
+                                {{ number_format($pending['total'], 0, ',', '.') }}€
+                            </h2>
+                            <small>+ {{ number_format($on_hold['total'], 0, ',', '.') }}€ en compte</small>
+                        </div>
                     </div>
                 </div>
 
