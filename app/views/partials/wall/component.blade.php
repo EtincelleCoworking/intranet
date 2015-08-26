35edcdff48072bd509d3c1de9fc3dc0fc7b32bc5
Sebastien Hordeaux <shordeaux@waterproof.fr>
2015-08-26 16:28:53 +0200

    cache issue

--- app/views/partials/wall/component.blade.php
+++ app/views/partials/wall/component.blade.php
@@ -4,7 +4,7 @@ if(empty($page_index)){
     $page_index = 0;
 }
 $isSuperAdmin = Auth::user()->isSuperAdmin();
-$cacheKey = sprintf('wall.%d', (bool)$isSuperAdmin, (int)$page_index);
+$cacheKey = sprintf('wall.%d.%d', (bool)$isSuperAdmin, (int)$page_index);
 $wallContent = Cache::get($cacheKey);
 if (empty($wallContent)) {
     $wallContent = View::make('partials.wall.inner', array('isSuperAdmin' => $isSuperAdmin, 'page_index' => $page_index))->render();
