a1988d1d40ce2a9a1ee76065dbf0cb213de57c41
Sebastien Hordeaux <shordeaux@waterproof.fr>
2015-09-05 18:29:00 +0200

    pdf config

--- app/config/packages/barryvdh/laravel-snappy/config.php
+++ app/config/packages/barryvdh/laravel-snappy/config.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltopdf',
+        'binary' => __DIR__.'/../../../../../vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
         'options' => array(),
     ),
     'image' => array(
