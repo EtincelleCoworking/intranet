b7cef4068f9dbf52d960f92a4b1063d7601fe6af
Sebastien Hordeaux <shordeaux@waterproof.fr>
2015-09-25 12:28:08 +0200

    cache issue on home "random" member

--- app/views/partials/member/component.blade.php
+++ app/views/partials/member/component.blade.php
@@ -5,7 +5,7 @@ $cacheKey = 'member';
 $boxContent = Cache::get($cacheKey);
 if (empty($boxContent)) {
     $boxContent = View::make('partials.member.inner')->render();
-    Cache::forever($cacheKey, $boxContent, 60);
+    Cache::remember($cacheKey, $boxContent, 60);
 }
 echo $boxContent;
 ?>
