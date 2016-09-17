604566532b293439a42d2e57d5355e9543a473ae
Sebastien Hordeaux <shordeaux@waterproof.fr>
2016-09-17 23:33:47 +0200

    tweaks

--- workbench/etincelle/booking/src/controllers/BookingOrderController.php
+++ workbench/etincelle/booking/src/controllers/BookingOrderController.php
@@ -9,17 +9,20 @@ class BookingOrderController extends Controller
 
     public function invoicing()
     {
-        $results = DB::select( DB::raw('SELECT users.id, users.firstname, users.lastname, users.email
+        $results = DB::select(DB::raw('SELECT users.id, users.firstname, users.lastname, users.email
          , (SELECT SUM(invoices_items.booking_hours) FROM invoices_items JOIN invoices ON invoices_items.invoice_id = invoices.id WHERE invoices.user_id = users.id) * 60 as quantity_ordered
          , (SELECT SUM(booking_item.duration) FROM booking_item JOIN booking ON booking_item.booking_id = booking.id WHERE booking.user_id = users.id AND booking_item.is_free = false)  as quantity_used
-         FROM users 
-         LEFT OUTER JOIN invoices ON invoices.user_id = users.id LEFT OUTER JOIN invoices_items ON invoices_items.invoice_id = invoices.id
-         LEFT OUTER JOIN booking ON booking.user_id = users.id 
-         WHERE (invoices_items.booking_hours IS NOT NULL OR booking.id IS NOT NULL) GROUP BY users.id ORDER BY users.lastname ASC, users.firstname ASC') );
+         FROM users ORDER BY users.lastname ASC, users.firstname ASC'));
         //var_dump($results);
         //exit;
         // durée de validité?
 
-        return View::make('booking::orders', array('items' => $results));
+        $items = array();
+        foreach ($results as $item) {
+            if ($item->quantity_ordered > 0 || $item->quantity_used > 0) {
+                $results[] = $item;
+            }
+        }
+        return View::make('booking::orders', array('items' => $items));
     }
 }
