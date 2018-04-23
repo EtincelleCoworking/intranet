64a1991e85bd794aa14c5584e71db27ca86caf3f
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-04-23 22:02:31 +0200

    monitoring

--- app/commands/CronRunCommand.php
+++ app/commands/CronRunCommand.php
@@ -615,7 +615,7 @@ group by booking.id
             join cities on locations.city_id = cities.id
           WHERE is_critical = 1 
             AND last_seen_at IS NOT NULL 
-            AND DATE_ADD(last_seen_at, INTERVAL 5 * frequency SECOND) < NOW())
+            AND DATE_ADD(last_seen_at, INTERVAL 5 * frequency SECOND) < NOW()
             AND (notified_at IS NULL OR (notified_at < last_seen_at))
           '));
         foreach ($items as $equipment) {
