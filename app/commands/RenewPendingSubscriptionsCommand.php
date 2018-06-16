1485a0e355c3c3c1e29ce6cbd23f53ef57108491
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-06-16 12:52:49 +0200

    subscription

---
+++ app/commands/RenewPendingSubscriptionsCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RenewPendingSubscriptionsCommand extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'etincelle:renew-pending-subscriptions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $subscriptions = Subscription::where('is_automatic_renew_enabled', '=', true)
+            ->where('renew_at', '<=', date('Y-m-d'))
+            ->get();
+        foreach ($subscriptions as $subscription) {
+            $invoice = $subscription->renew();
+
+            $data = array();
+            $data['text'] = sprintf('La facture <%s|%s> de renouvellement d\'abonnement de %s a été créée automatiquement',
+                URL::route('invoice_modify', $invoice->id), $invoice->ident);
+            $this->slack(Config::get('etincelle.slack_staff_toulouse'), $data);
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array();
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array();
+    }
+
+}
---
+++ app/commands/SendUpcomingSubscriptionRenewNotificationsCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class SendUpcomingSubscriptionRenewNotificationsCommand extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'etincelle:send-upcoming-subscription-renew-notification';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $subscriptions = Subscription::where('is_automatic_renew_enabled', '=', true)
+            ->where('renew_at', '>', date('Y-m-d'))
+            ->where('renew_at', '<=', date('Y-m-d', strtotime('+7 days')))
+            ->whereNull('reminded_at')
+            ->get();
+        foreach ($subscriptions as $subscription) {
+            Mail::send('emails.upcoming_subscription_renew', array('subscription' => $subscription), function ($message) use ($subscription) {
+                $message->from($_ENV['mail_address'], $_ENV['mail_name'])
+                    ->bcc($_ENV['mail_address'], $_ENV['mail_name']);
+
+                $message->to($subscription->user->email, $subscription->user->fullname);
+                $message->subject(sprintf('%s - Renouvellement de votre abonnement le %s', $_ENV['organisation_name'], date('d/m/Y', $subscription->renew_at)));
+            });
+            $subscription->reminded_at = date('Y-m-d');
+            $subscription->save();
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array();
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array();
+    }
+
+}
