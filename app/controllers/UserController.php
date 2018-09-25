907d6e255623f403b68783df5040a75636031f33
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-09-25 13:27:38 +0200

    login issue

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -30,7 +30,7 @@ class UserController extends BaseController
         $result = Auth::attempt($user, Input::get('remember'));
         if ($result) {
             if (Auth::user()->is_enabled) {
-                return Redirect::intended('dashboard');
+                return Redirect::intended(URL::route('dashboard'));
             }
             return Redirect::route('user_login')->with('mError', 'Connexion impossible, merci de contacter un administrateur')->withInput();
         } else {
