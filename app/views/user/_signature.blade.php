b14fe42da288b6dd53f54779dfb49153072147c7
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-10-05 14:53:13 +0200

    signature

--- app/views/user/_signature.blade.php
+++ app/views/user/_signature.blade.php
@@ -20,7 +20,9 @@
                         <p style="margin:0px">
  <span style="display:inline-block">
 T&eacute;l. : <span style="color:#4F2067;font-family:sans-serif">{{$_ENV['organisation_phone']}}</span>
-Mobile : <span style="color:#4F2067;font-family:sans-serif">{{$user->phoneFmt}}</span>
+     @if($user->phone)
+         Mobile : <span style="color:#4F2067;font-family:sans-serif">{{$user->phoneFmt}}</span>
+     @endif
 </span>
                         </p>
                     </td>
@@ -33,7 +35,7 @@ Mobile : <span style="color:#4F2067;font-family:sans-serif">{{$user->phoneFmt}}<
                 <table border="0" cellpadding="0" cellspacing="0">
                     <tbody>
                     <tr style="padding-top:10px">
-                        <td  width="80%" align="left" style="padding-right:5px;text-align:left;padding-top:0px">
+                        <td width="80%" align="left" style="font:12px Arial;color:rgb(100,100,100);padding-right:5px;text-align:left;padding-top:0px">
                             <span><b>{{$_ENV['organisation_name']}}</b></span><br/>
                             <span>
                     {{$user->location->address}}
@@ -47,7 +49,7 @@ Mobile : <span style="color:#4F2067;font-family:sans-serif">{{$user->phoneFmt}}<
                             </p>
 
                         </td>
-                        <td  width="20%" align="right" style="padding-right:5px;text-align:right;padding-top:0px">
+                        <td width="20%" align="right" style="padding-right:5px;text-align:right;padding-top:0px">
                             @if($twitter)
                                 <a href="https://twitter.com/{{$twitter}}" target="_blank">
                                     <img style="border-radius:0px;border:0px"
