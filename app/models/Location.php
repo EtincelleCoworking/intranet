33d47c66b931998217ac57f3e906c706e259ca37
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-10-10 14:58:46 +0200

    stats

--- app/models/Location.php
+++ app/models/Location.php
@@ -107,7 +107,7 @@ class Location extends Eloquent
                 '2018-02' => 17685 + 4480,
                 '2018-08' => 16265 + 4060,// - Caroline
                 '2018-09' => 17685 + 4480 - 1080, // Julie partiel
-                '2018-10' => 17685 + 4480,
+                '2018-10' => 17405 + 4480,
             ),
             //'Toulouse > Espace W' => array(),
             'Alsace Lorraine' => array(
