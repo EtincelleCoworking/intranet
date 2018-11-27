8efd53d56c59bbc89a3f97eb099bb8b94ab2204a
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-11-27 14:53:17 +0100

    voucher

--- app/controllers/PostboxController.php
+++ app/controllers/PostboxController.php
@@ -88,7 +88,7 @@ class PostboxController extends BaseController
             $item->kind_id = $data['kind'][$index];
             $item->from_name = $data['from_name'][$index];
             $item->details = $data['details'][$index];
-            $item->is_important = $data['is_important'][$index];
+            $item->is_important = isset($data['is_important'][$index])?$data['is_important'][$index]:false;
             $item->save();
 
             $items[] = $item;
