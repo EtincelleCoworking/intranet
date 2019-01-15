009c2e3fc1eebbcd7d707c7c32e63cdd766ff2cc
Sebastien Hordeaux <shordeaux@waterproof.fr>
2019-01-15 16:45:07 +0100

    aircall

--- app/controllers/AircallController.php
+++ app/controllers/AircallController.php
@@ -15,19 +15,59 @@ class AircallController extends BaseController
         return preg_replace('/[^0-9]/', '', $value);
     }
 
-    public function webhook($phone)
+    protected function sendInsightCard($json)
+    {
+        $uri = sprintf('https://%s:%s@api.aircall.io/v1/calls/%s/insight_cards',
+            $_ENV['aircall_id'], $_ENV['aircall_secret'], $json->data->id);
+
+        $data = json_encode( [
+            'call_id' => $json->data->id,
+            'creationDate' => time(),
+            'contents' => [
+                [
+                    'type' => 'title',
+                    'text' => 'TITLE',
+                    'link' => 'https://www.google.com/'
+                ],
+                [
+                    'type' => 'shortText',
+                    'text' => 'SHORT_TEXT',
+                    'label' => 'LABEL',
+                    'link' => 'https://www.google.com/'
+                ]
+            ]
+        ]);
+        $ch = curl_init($uri);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+        );
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+    public function webhook()
     {
         $json = json_decode(Request::getContent());
+        $phone = $json->data->raw_digits;
         $data['call'] = $json;
 
-        $user = User::where('phone', $this->parsePhoneNumber($json->data->raw_digits))
+        $user = User::where('phone', $this->parsePhoneNumber($phone))
             ->with('organisations')
             ->first();
         $data['user'] = null;
+        $one_year_ago = date('Y-m-d', strtotime('-1 year'));
+
         if ($user) {
+            $this->sendInsightCard($json);
+
             $data['user'] = [
                 'id' => $user->id,
-                'firstname' => $user->firtname,
+                'firstname' => $user->firstname,
                 'lastname' => $user->lastname,
                 'email' => $user->email
             ];
@@ -42,37 +82,49 @@ class AircallController extends BaseController
                 ];
             }
             $data['invoices'] = [];
-            foreach (Invoice::where('type', 'F')->where('user_id', $user->id)->orderBy('date_invoice', 'DESC')->with('items')->get() as $invoice) {
+            foreach (Invoice::where('type', 'F')->where('user_id', $user->id)->where('date_invoice', '>', $one_year_ago)->orderBy('date_invoice', 'DESC')->with('items')->get() as $invoice) {
                 $data['invoices'][] = [
                     'id' => $invoice->id,
                     'reference' => $invoice->ident,
                     'occurs_at' => $invoice->date_invoice,
                     'paid_at' => $invoice->date_payment,
-                    'amount' => $invoice->TotalWithTaxes()
+                    'amount' => $invoice->totalWithTaxes,
+                    'edit_link' => route('invoice_modify', array('id' => $invoice->id))
                 ];
             }
             $data['quotes'] = [];
-            foreach (Invoice::where('type', 'D')->where('user_id', $user->id)->orderBy('date_invoice', 'DESC')->with('items')->get() as $invoice) {
+            foreach (Invoice::where('type', 'D')->where('user_id', $user->id)->where('date_invoice', '>', $one_year_ago)->orderBy('date_invoice', 'DESC')->with('items')->get() as $invoice) {
                 $data['quotes'][] = [
                     'id' => $invoice->id,
                     'reference' => $invoice->ident,
                     'occurs_at' => $invoice->date_invoice,
                     'paid_at' => $invoice->date_payment,
-                    'amount' => $invoice->TotalWithTaxes()
+                    'amount' => $invoice->totalWithTaxes,
+                    'edit_link' => route('invoice_modify', array('id' => $invoice->id))
                 ];
             }
             $data['bookings'] = [];
-            foreach (BookingItem::where('user_id', $user->id)->orderBy('date_invoice', 'DESC')->with('ressource')->with('ressource.location')->with('ressource.location.city')->get() as $item) {
-                [
-                    [
-                        'id' => $item->id,
-                        'start_at' => $item->start_at,
-                        'end_at' => date('Y-m-d H:i:s', strtotime($item->start_at) + $item->duration * 60),
-                        'location' => $item->ressource->location->city->name,
-                        'room' => $item->ressource->name,
-                        'participants' => $item->participant_count,
-                        'is_confirmed' => $item->confirmed_at != null
-                    ]];
+            foreach (BookingItem::join('booking', 'booking.id', '=', 'booking_item.booking_id')->where('booking.user_id', $user->id)->where('start_at', '<', date('Y-m-d'))->limit(5)->orderBy('start_at', 'DESC')->with('ressource')->with('ressource.location')->with('ressource.location.city')->get() as $item) {
+                $data['bookings'][] = [
+                    'id' => $item->id,
+                    'start_at' => $item->start_at,
+                    'end_at' => date('Y-m-d H:i:s', strtotime($item->start_at) + $item->duration * 60),
+                    'location' => $item->ressource->location->city->name,
+                    'room' => $item->ressource->name,
+                    'participants' => $item->participant_count,
+                    'is_confirmed' => $item->confirmed_at != null
+                ];
+            }
+            foreach (BookingItem::join('booking', 'booking.id', '=', 'booking_item.booking_id')->where('booking.user_id', $user->id)->where('start_at', '>', date('Y-m-d'))->orderBy('start_at', 'DESC')->with('ressource')->with('ressource.location')->with('ressource.location.city')->limit(5)->get() as $item) {
+                $data['bookings'][] = [
+                    'id' => $item->id,
+                    'start_at' => $item->start_at,
+                    'end_at' => date('Y-m-d H:i:s', strtotime($item->start_at) + $item->duration * 60),
+                    'location' => $item->ressource->location->city->name,
+                    'room' => $item->ressource->name,
+                    'participants' => $item->participant_count,
+                    'is_confirmed' => $item->confirmed_at != null
+                ];
             }
 
             $data['previous_calls'] = [/*
@@ -89,18 +141,21 @@ class AircallController extends BaseController
             ]
 */];
             $options = array(
-                'cluster' => $_ENV['PUSHER_APP_CLUSTER'],
+                'cluster' => $_ENV['pusher_app_cluster'],
                 'useTLS' => true,
                 'debug' => true
             );
             $pusher = new \Pusher\Pusher(
-                $_ENV['PUSHER_APP_KEY'],
-                $_ENV['PUSHER_APP_SECRET'],
-                $_ENV['PUSHER_APP_ID'],
+                $_ENV['pusher_app_key'],
+                $_ENV['pusher_app_secret'],
+                $_ENV['pusher_app_id'],
                 $options
             );
             $result = $pusher->trigger('aircall', 'call.created', $data);
             var_dump($result);
+            echo '<pre>';
+            echo json_encode($data, JSON_PRETTY_PRINT);
+            echo '</pre>';
             //echo json_encode($data);
         }
     }
