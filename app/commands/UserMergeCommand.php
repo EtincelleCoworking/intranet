9fbe58b044164a9ad0593a03618855e218cbe377
Sébastien Hordeaux <shordeaux@waterproof.fr>
2022-05-17 09:44:28 +0200

    user merge

--- app/commands/UserMergeCommand.php
+++ app/commands/UserMergeCommand.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -40,11 +41,53 @@ class UserMergeCommand extends Command
      */
     public function fire()
     {
-        $user = User::where('email', '=', $this->argument('recent_email'))->first();
+        $email = $this->argument('recent_email');
+        $user = User::where('email', '=', $email)->first();
+        if (!$user) {
+            $this->output->writeln(sprintf('<error>Unable to find user %s</error>', $email));
+            return false;
+        }
         $this->output->writeln(sprintf('User %s (ID = %d)', $user->email, $user->id));
 
-        $olds = $this->option('old_email');
-        dd($olds);
+        $old_emails = $this->option('old');
+        $olds = User::whereIn('email', $old_emails)->get();
+
+        $old_ids = array();
+        foreach ($olds as $old) {
+            $index = array_search($old->email, $old_emails);
+            if ($index !== false) {
+                unset($old_emails[$index]);
+            }
+            $old_ids[] = $old->id;
+        }
+        if (count($old_emails) > 0) {
+            $this->output->writeln(sprintf('<error>Unable to find user%s %s</error>',
+                (count($old_emails) == 1) ? '' : 's', implode(',', $old_emails)));
+            return false;
+        }
+        DB::transaction(function () use ($user, $old_ids) {
+            DB::table('booking')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('booking_item')->update(['confirmed_by_user_id' => $user->id])->whereIn('confirmed_by_user_id', $old_ids);
+            DB::table('devices')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('door_tokens')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('gift_photoshoot_slot')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('invoices')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('invoices_comments')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('invoices_items')->update(['subscription_user_id' => $user->id])->whereIn('subscription_user_id', $old_ids);
+            DB::table('locker_history')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('organisation_user')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('past_times')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('phonebox_session')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('skills')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('subscription')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('team_planning_item')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('users')->update(['affiliate_user_id' => $user->id])->whereIn('affiliate_user_id', $old_ids);
+            DB::table('user_gift')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('user_hashtag')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('user_job')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('wall_posts')->update(['user_id' => $user->id])->whereIn('user_id', $old_ids);
+            DB::table('user')->whereIn('id', $old_ids)->delete();
+        });
     }
 
 
@@ -68,7 +111,7 @@ class UserMergeCommand extends Command
     protected function getOptions()
     {
         return array(
-            array('old_email', null, InputOption::VALUE_IS_ARRAY, '', null),
+            array('old', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, '', null),
         );
     }
 
