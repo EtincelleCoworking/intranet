ce580398790399ca6a3a6389bee3e0121f0c9d19
Sébastien Hordeaux <shordeaux@waterproof.fr>
2024-01-25 12:50:42 +0100

    coffeeshop

--- app/models/InvoicingRuleProcessor/MeetingRoomDiscount10.php
+++ app/models/InvoicingRuleProcessor/MeetingRoomDiscount10.php
@@ -5,6 +5,7 @@ class InvoicingRuleProcessor_MeetingRoomDiscount10 extends InvoicingRuleProcesso
     public function execute($invoice_lines)
     {
         $result = array();
+        $discount = $this->getDiscountRate();
         foreach ($invoice_lines as $line) {
             $result[] = $line;
             if ($line->ressource_id && ($line->ressource->ressource_kind_id == RessourceKind::TYPE_MEETING_ROOM)
@@ -12,18 +13,20 @@ class InvoicingRuleProcessor_MeetingRoomDiscount10 extends InvoicingRuleProcesso
 
                 $result[] = $this->createDiscountLine($line,
                     sprintf('Réduction commerciale - %s (-%s%%)',
-                        $line->ressource->name, static::DISCOUNT_RATE), -static::DISCOUNT_RATE / 100 * $line->amount);
+                        $line->ressource->name, $discount), -$discount / 100 * $line->amount);
             }
         }
 
         return $result;
     }
 
-    const DISCOUNT_RATE = 10;
+    protected function getDiscountRate(){
+        return 10;
+    }
 
     public static function getCaption()
     {
-        return sprintf('%d%% de réduction sur les salles de réunion', static::DISCOUNT_RATE);
+        return sprintf('%d%% de réduction sur les salles de réunion', $this->getDiscountRate());
     }
 
 }
---
+++ app/models/InvoicingRuleProcessor/MeetingRoomDiscount14.php
@@ -0,0 +1,8 @@
+<?php
+
+class InvoicingRuleProcessor_MeetingRoomDiscount14 extends InvoicingRuleProcessor_MeetingRoomDiscount10
+{
+    protected function getDiscountRate(){
+        return 14;
+    }
+}
