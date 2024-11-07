ac481b9607a222b1ad8c72356a43595c8e4ca0b7
Sébastien Hordeaux <shordeaux@waterproof.fr>
2024-11-07 10:47:09 +0100

    coffeeshop

--- app/views/partials/next_birthday/component.blade.php
+++ app/views/partials/next_birthday/component.blade.php
@@ -17,7 +17,7 @@ if (empty($cacheContent)) {
                 YEAR) ASC')
             ->limit(5)->get();
     if (count($users) > 0) {
-        $cacheContent = View::make('partials.next_birthday.inner', array('users' => $users))->render();
+        $cacheContent = View::make('partials.next_birthday.inner.r2024-11-07', array('users' => $users))->render();
         Cache::put($cacheKey, $cacheContent, new \dateTime(date('Y-m-d 00:00:00', strtotime('tomorrow'))));
     }
 }
