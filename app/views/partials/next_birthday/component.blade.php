64367ad2cd7142ef4fdcf4cdb93037b47cc16033
Sébastien Hordeaux <shordeaux@waterproof.fr>
2024-11-07 10:59:54 +0100

    coffeeshop

--- app/views/partials/next_birthday/component.blade.php
+++ app/views/partials/next_birthday/component.blade.php
@@ -5,16 +5,9 @@ $cacheKey = 'birthday';
 $cacheContent = Cache::get($cacheKey);
 if (empty($cacheContent)) {
     $users = User::where('birthday', '<>', '0000-00-00')
-            ->whereRaw('DATE_ADD(birthday,
-                INTERVAL YEAR(CURDATE())-YEAR(birthday)
-                         + IF(DAYOFYEAR(CURDATE()) > DAYOFYEAR(birthday),1,0)
-                YEAR)
-            BETWEEN DATE_SUB(CONCAT(CURDATE(), \' 00:00:00\'), INTERVAL 2 DAY) AND DATE_ADD(CURDATE(), INTERVAL 60 DAY)')
+            ->whereRaw('DAYOFYEAR(birthday) BETWEEN DAYOFYEAR(CURDATE())-1 AND DAYOFYEAR(CURDATE()) + 60')
             ->whereIsMember(true)
-            ->orderByRaw('DATE_ADD(birthday,
-                INTERVAL YEAR(CURDATE())-YEAR(birthday)
-                         + IF(DAYOFYEAR(CURDATE()) > DAYOFYEAR(birthday),1,0)
-                YEAR) ASC')
+            ->orderByRaw('DAYOFYEAR(birthday) ASC')
             ->limit(5)->get();
     if (count($users) > 0) {
         $cacheContent = View::make('partials.next_birthday.inner', array('users' => $users))->render();
