51a2d89d9e7ee3ddb306815be0bf9fb71954532e
Sébastien Hordeaux <shordeaux@waterproof.fr>
2025-11-13 09:41:35 +0100

    forecast

--- app/commands/CatalyzSyncCommand.php
+++ app/commands/CatalyzSyncCommand.php
@@ -41,6 +41,7 @@ class CatalyzSyncCommand extends Command
         // https://etincelle-coworking.catalyz.fr/api/invoices
         $json = [];
         $uri = sprintf('%s?count=100000&paid_at[lte]=%s&paid_at[gte]=%s', $this->argument('api_uri'), date('Y-m-d'), date('Y-m-d', strtotime('-1 month')));
+        $this->output->writeln(sprintf('API : %s', $uri));
 
         foreach (json_decode(file_get_contents($uri)) as $invoice) {
             $json[$invoice->reference] = $invoice;
